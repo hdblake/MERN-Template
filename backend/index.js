fac867dc83efa129bdd65fdcdf59b5f75606c2eb
= <=>
2024-01-29 14:42:35 -0600

    updated mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
+// MAKE SURE TO MOVE MONGO_URI CONNECTION AND PORT TO SEPARATE .env FILE WITH CORRECT CONNECTION
+// MONGO_URI=mongodb+srv://<username>:<password>@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// PORT=3080
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3080;
-const mongoDb = require("./db/connect");
+const mongoose = require("mongoose");
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 app.use("/", require("./routes"));
 
-mongoDb.initDb((err, mongoDb) => {
-	if (err) {
-		console.log(err);
-	} else {
-		app.listen(port, () => {
-			console.log(`Connected to Db. Listening on port: ${port}`);
-		});
-	}
+mongoose.connect(process.env.MONGO_URI).then(() => {
+	app.listen(port, () => {
+		console.log(`Server listening on port: ${port}!`);
+	});
 });
